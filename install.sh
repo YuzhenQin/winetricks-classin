7ea24e553185e70b06ce5ef109e3c09ceb985941
Qin Yuzhen <1@jin-dan.site>
2022-06-03 17:06:19 +0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 更新索引
+sudo apt-get update
+
+# 安装证书
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    wget
+
+# 换源
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse" > tmpsrc
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse" >> tmpsrc
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse" >> tmpsrc
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse" >> tmpsrc
+sudo cp tmpsrc /etc/apt/sources.list
+rm tmpsrc
+
+# 更新索引
+sudo apt-get update
+
+# 安装 wine 和 winetricks
+sudo apt-get install -y wine winetricks
+
+# 安装 classin
+wget https://yuzhenqin.github.io/winetricks-classin/classin.verb
+sudo winetricks --isolate --verbose ./classin.verb
+rm ./classin.verb
